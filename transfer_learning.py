d06cef68bfc7f1e5918f54a818dada0082e079bc
Mehdi Hosseini <mehdy.h59@gmail.com>
2024-11-27 15:04:06 +0000

    data transformation

--- transfer_learning.py
+++ transfer_learning.py
@@ -9,3 +9,22 @@ import time
 import os
 import copy
 
+# data prep
+
+mean = np.array([0.5,0.5,0.5])
+std = np.array([0.25,0.25,0.25])
+
+data_transform = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean,std)
+    ])
+}
