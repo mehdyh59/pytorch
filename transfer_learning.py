595ab3910ebc2dea434d3d2c18034d934c230f30
Mehdi Hosseini <mehdy.h59@gmail.com>
2024-11-27 14:53:03 +0000

    Transfer Learning using pre-trained ResNet18 CNN, trained by 1m from image net data set to classify images to 1000s categories

---
+++ transfer_learning.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import datasets, models, transforms
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
