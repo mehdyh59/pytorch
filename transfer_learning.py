11ade8cf17dfe7dd4714f0044b807e43d48b5faa
Mehdi Hosseini <mehdy.h59@gmail.com>
2024-11-28 10:25:46 +0000

    visulise a batch of training pictures

--- transfer_learning.py
+++ transfer_learning.py
@@ -9,12 +9,16 @@ import time
 import os
 import copy
 
-# data prep
-
+##data prep
+# vars
 mean = np.array([0.5,0.5,0.5])
 std = np.array([0.25,0.25,0.25])
+data_dir = "./data/hymenoptera_data"
+batch_size = 4
 
-data_transform = {
+
+#transformation
+data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
@@ -28,3 +32,34 @@ data_transform = {
         transforms.Normalize(mean,std)
     ])
 }
+
+#datasets and dataloaders
+image_datasets={x: datasets.ImageFolder(os.path.join(data_dir,x), data_transforms[x])
+                for x in ['train','val']}
+
+data_loaders={x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True)
+              for x in ['train','val'] }
+
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
+class_names = image_datasets['train'].classes
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def imshow(inp, title):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    plt.title(title)
+    plt.show()
+
+
+# Get a batch of training data
+inputs, classes = next(iter(data_loaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
